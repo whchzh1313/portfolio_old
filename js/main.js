0a17a5fdef8e6bd210331be34a1ddc9a64eda5e8
whchzh1313 <79441700+whchzh1313@users.noreply.github.com>
2021-03-09 16:22:29 +0900

    포트폴리오
    2021-03-09 PM 4:22

--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
         }, 1400);
     }
     loadEvent();
-    _gallery.find('li.gal_show').eq(3).addClass('gal_mr');
-    _gallery.find('li.gal_show').eq(7).addClass('gal_mr');
+    _gallery.find('li').eq(3).addClass('gal_mr');
+    _gallery.find('li').eq(7).addClass('gal_mr');
 
     // 1] nav li 클릭시 page 이동 = margin-left - i"00vw"
     var _navBtn = $('#nav ul li a');
     var pageIdx
     _navBtn.on('click', function () {
         var pageIdx = $(this).parent('li').index();
-        
+
         _loadBg.fadeIn(400);
         setTimeout(function () {
 
@@ -38,6 +38,7 @@ $(document).ready(function () {
                 _navBtn.removeClass('view_page view_page_orange');
                 _navBtn.eq(pageIdx).addClass('view_page');
             }
+
             function makeViewPageOrange() {
                 _navBtn.removeClass('view_page view_page_orange');
                 _navBtn.eq(pageIdx).addClass('view_page_orange');
@@ -67,7 +68,7 @@ $(document).ready(function () {
 
     });
     // 1-1] resize 이벤트가 안됩니다!
-    $(window).resize( function(){
+    $(window).resize(function () {
         if ($('#section .page').width() > 1400) {
             $('#section').css({
                 marginLeft: "-" + pageIdx + "00vw"
@@ -80,24 +81,42 @@ $(document).ready(function () {
     });
     // 2] page2 화면 생성이펙트
     // 3] page3 포트폴리오 페이지 이동
-    
+
     // 4] page4 design 필터에 따라 보여지는 이미지 교체
     _filter.on('click', function (e) {
         e.preventDefault();
         console.log($(this).children());
         if ($(this).index() === 0) {
-            _gallery.find('li').removeClass('gal_show').removeClass('gal_mr').siblings('.gal_banner').addClass('gal_show');
+            _gallery.find('li').fadeOut(600, function() {
+                _gallery.find('li').removeClass('gal_mr')
+            }).delay(800).siblings('.gal_banner').fadeIn(600, function () {
+                _gallery.find('.gal_banner').eq(3).addClass('gal_mr');
+            });
+            
 
         } else if ($(this).index() === 1) {
-            _gallery.find('li').removeClass('gal_show').removeClass('gal_mr').siblings('.gal_event').addClass('gal_show');
+            _gallery.find('li').fadeOut(600, function() {
+                _gallery.find('li').removeClass('gal_mr')
+            }).delay(800).siblings('.gal_event').fadeIn(600, function () {
+                _gallery.find('.gal_event').eq(3).addClass('gal_mr');
+            });
+            
 
         } else if ($(this).index() === 2) {
-            _gallery.find('li').removeClass('gal_show').removeClass('gal_mr').siblings('.gal_design').addClass('gal_show');
+            _gallery.find('li').fadeOut(600, function() {
+                _gallery.find('li').removeClass('gal_mr');
+            }).delay(800).siblings('.gal_design').fadeIn(600, function () {
+                _gallery.find('.gal_design').eq(3).addClass('gal_mr');
+            });
+            
 
         }
-        
-        _gallery.find('li.gal_show').eq(3).addClass('gal_mr');
-        _gallery.find('li.gal_show').eq(7).addClass('gal_mr');
+
+        function galleryMr() {
+           eq(3).addClass('gal_mr');
+           eq(7).addClass('gal_mr');
+        }
+
 
         _filter.children().removeClass('view_page');
         $(this).children().addClass('view_page');
